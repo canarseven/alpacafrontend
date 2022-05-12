8935144d8c48f8216cd06e29d9a6f171e1f4ea2d
Can Arseven <can@arseven.at>
2022-05-12 12:32:34 +0200

    Setup minimal working version

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("sections/<int:num>", views.section, name='section'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,20 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 
+import datetime
+import os
+import sys
+
+import alpaca_trade_api as tradeapi
+import requests
+import pandas as pd
+
+
+def initialize_api():
+    try:
+        api = tradeapi.REST()
+        return api
+    except:
+        print("Error in api init ...")
+        print("Exciting program.")
+        sys.exit()
+
+
+def check_account_status(api):
+    account = api.get_account()
+    return account.status
+
+
+def check_positions(api):
+    account = api.get_account()
+    positions = api.list_positions()
+
+    current_portfolio_val = account.portfolio_value
+    current_cash = account.cash
+
+    formatted_positions = []
+    for position in positions:
+        formatted_position = {
+            "symbol": position.symbol,
+            "market_value": round(float(position.market_value), 2),
+            "unrealized_plpc": round(float(position.unrealized_plpc), 2) * 100,
+            "unrealized_intraday_plpc": round(float(position.unrealized_intraday_plpc), 2) * 100,
+            "share_of_portfolio": round(float(position.market_value) / float(current_portfolio_val), 2) * 100
+        }
+        formatted_positions.append(formatted_position)
+    formatted_positions = pd.DataFrame(formatted_positions)
+    formatted_positions.sort_values(by="unrealized_plpc")
+    positions_data = {"positions": formatted_positions,
+                      "portfolio_val": round(float(current_portfolio_val), 2),
+                      "cash": round(float(current_cash), 2)}
+    return positions_data
+
 
 # Create your views here.
 def index(request):
-    return render(request, "frontend/index.html")
-
-
-texts = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor mauris, maximus semper volutpat vitae, varius placerat dui. Nunc consequat dictum est, at vestibulum est hendrerit at. Mauris suscipit neque ultrices nisl interdum accumsan. Sed euismod, ligula eget tristique semper, lecleo mi nec orci. Curabitur hendrerit, est in ",
-    "Praesent euismod auctor quam, id congue tellus malesuada vitae. Ut sed lacinia quam. Sed vitae mattis metus, vel gravida ante. Praesent tincidunt nulla non sapien tincidunt, vitae semper diam faucibus. Nulla venenatis tincidunt efficitur. Integer justo nunc, egestas eget dignissim dignissim,  facilisis, dictum nunc ut, tincidunt diam.",
-    "Morbi imperdiet nunc ac quam hendrerit faucibus. Morbi viverra justo est, ut bibendum lacus vehicula at. Fusce eget risus arcu. Quisque dictum porttitor nisl, eget condimentum leo mollis sed. Proin justo nisl, lacinia id erat in, suscipit ultrices nisi. Suspendisse placerat nulla at volutpat ultricies"]
-
-
-def section(request, num):
-    if 1 <= num <= 3:
-        return HttpResponse(texts[num - 1])
-    else:
-        raise Http404("No such section")
+    api = initialize_api()
+    positions_data = check_positions(api)
+    return render(request, "frontend/index.html", positions_data)
